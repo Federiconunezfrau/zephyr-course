e282b0f5029da905b260630e917b156d3c0f0e74
Federiconunezfrau <federiconunezfrau@yahoo.com>
2026-05-12 21:21:19 -0300

    Se agregó el llamado a la función de extensión en el main

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,6 +2,9 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
+// Header file de mi driver
+#include <my_sensor/my_sensor.h>
+
 #define SLEEP_TIME_MS CONFIG_APP_HEARTBEAT_PERIOD_MS
 
 static const struct device *led_sensor = DEVICE_DT_GET_ANY(zephyr_my_sensor);
@@ -12,6 +15,7 @@ int main(void)
 {
     bool led_state = true;
     struct sensor_value dummy;
+    int count = 0;
 
     if (!device_is_ready(led_sensor)) {
         return 0;
@@ -22,13 +26,15 @@ int main(void)
         if(led_state) {
             // Apaga el LED
             sensor_channel_get(led_sensor, SENSOR_CHAN_ALL, &dummy);
+            count++;
+            my_sensor_set_count(led_sensor, count);
         }
         else {
             // Enciende el LED
             sensor_sample_fetch(led_sensor);
         }
         led_state = !led_state;        
-        LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
+        LOG_INF("LED state: %s, count: %d", led_state ? "ON" : "OFF", count);
         k_msleep(SLEEP_TIME_MS);
     }
     return 0;
