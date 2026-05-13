a6d7a8721ece209097358042936cae5591db64ec
Federiconunezfrau <federiconunezfrau@yahoo.com>
2026-05-12 21:21:19 -0300

    Se agregó el llamado a la función de extensión en el main

--- my_driver_module/include/my_sensor/my_sensor.h
+++ my_driver_module/include/my_sensor/my_sensor.h
@@ -1,6 +1,14 @@
 #ifndef ZEPHYR_DRIVERS_SENSOR_MY_SENSOR_H_
 #define ZEPHYR_DRIVERS_SENSOR_MY_SENSOR_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+int my_sensor_set_count(const struct device *dev, int count);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* ZEPHYR_DRIVERS_SENSOR_MY_SENSOR_H_ */
