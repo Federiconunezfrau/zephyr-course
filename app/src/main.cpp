2c8b5ff5fdea9a3045a7453d10108ffe548305e9
Federiconunezfrau <federiconunezfrau@yahoo.com>
2026-05-16 21:36:35 -0300

    Se agregó el comando sensor set_count para setera el valor del atributo count del dispositivo. Se agregaron validaciones para evitar que se pasen valores que no corresponden. La validación del argc la hace zephyr ya que se utilizó SHELL_CMD_ARG

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -12,6 +12,41 @@ static const struct device *led_sensor = DEVICE_DT_GET_ANY(zephyr_my_sensor);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
+#include <stdlib.h>
+#include <stdint.h>
+#include <limits.h>
+
+// Función auxiliar para validar argumento y convertir string en int
+static bool myStringToInt(const char *string, int *val) {
+
+    char *temp;
+    long result;
+    
+    result = strtol(string, &temp, 10);
+
+    if (*temp && *temp != '\n')
+    {
+        return false;
+    }
+
+    // Se valida que no esté por fuera del rango de long
+    if( errno == ERANGE ) {
+        errno = 0;
+        return false;
+    }
+    
+    // Como strtol es para convertir a long, antes de retornar se valida que esté dentro de
+    // los límites de un entero
+    if (result < INT_MIN || result > INT_MAX)
+    {
+        return false;
+    }
+    
+    *val = (int)result;
+
+    return true;
+}
+
 // Handler para el sub comando sensor fetch
 static int cmd_sensor_fetch(const struct shell *sh, size_t argc, char **argv) {
     
@@ -47,6 +82,27 @@ static int cmd_sensor_info(const struct shell *sh, size_t argc, char **argv) {
     return 0;
 }
 
+// Handler para el sub comando sensor set
+static int cmd_sensor_set_count(const struct shell *sh, size_t argc, char **argv) {
+    
+    int count;
+    
+    ARG_UNUSED(argc);
+
+    // Hay que validar que es un entero positivo
+    if( (myStringToInt(argv[1], &count) == false ) || (count < 0) ) {
+        shell_error(sh, "Ingrese un número entero entre 0 y %d", INT_MAX);
+        return 0;
+    }
+
+    // Se setea el campo
+    my_sensor_set_count(led_sensor, count);
+
+    // Se imprime un mensaje con el valor.
+    shell_print(sh, "Count = %d", count);
+    return 0;
+}
+
 int main(void)
 {
     if (!device_is_ready(led_sensor)) {
@@ -60,11 +116,12 @@ int main(void)
     return 0;
 }
 
-// Se define la lista de subcomandos
+// Se define la lista de subcomandos de sensor
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_sensor,
     SHELL_CMD(fetch, NULL, "Turns LED ON", cmd_sensor_fetch),
     SHELL_CMD(read , NULL, "Turns LED OFF", cmd_sensor_read),
     SHELL_CMD(info , NULL, "Imprime el nombre del dispositivo y el ready state", cmd_sensor_info),
+    SHELL_CMD_ARG(set_count, NULL, "Permite setear atributos del dispositivo", cmd_sensor_set_count, 2, 0),
     SHELL_SUBCMD_SET_END
 );
 
